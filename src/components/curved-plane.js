39159461be2a3812d486e7d6528a350ae2b439aa
Byambaa <byambag12@gmail.com>
2021-08-27 16:30:53 +0800

    changes

--- src/components/curved-plane.js
+++ src/components/curved-plane.js
@@ -13,19 +13,20 @@ module.exports = AFRAME.registerComponent('curved-plane', {
         let mesh = this.el.getObject3D('mesh');
         let width = this.el.getAttribute('width');
         let height = this.el.getAttribute('height');
-        let browser_pane = new THREE.PlaneGeometry(width, height, 5, 1);
-        let curve = new THREE.CubicBezierCurve3(
-            browser_pane.vertices[0],
-            new THREE.Vector3(0.375*width, 0, -0.06*width ),
-            new THREE.Vector3(0.625*width, 0, -0.06*width ),
-            browser_pane.vertices[(browser_pane.vertices.length/2) - 1]
-        );
-        let planePoints = curve.getPoints(Math.abs(browser_pane.vertices.length/2)-1);
-        for (let edgeI = 1; edgeI < 3; edgeI++) {
-            for (let pointI = 0; pointI < planePoints.length; pointI++) {
-                browser_pane.vertices[(edgeI === 2 ? planePoints.length + pointI : pointI)].z = planePoints[pointI].z;
-            }
-        }
+        let browser_pane = new THREE.SphereGeometry(2.5, 16, 16, 0, 3.1);
+        // let curve = new THREE.CubicBezierCurve3(
+        //     browser_pane.vertices[0],
+        //     new THREE.Vector3(0.375*width, 0, -0.06*width ),
+        //     new THREE.Vector3(0.625*width, 0, -0.06*width ),
+        //     browser_pane.vertices[(browser_pane.vertices.length/2) - 1]
+        // );
+        // let planePoints = curve.getPoints(Math.abs(browser_pane.vertices.length/2)-1);
+        // for (let edgeI = 1; edgeI < 3; edgeI++) {
+        //     for (let pointI = 0; pointI < planePoints.length; pointI++) {
+        //         browser_pane.vertices[(edgeI === 2 ? planePoints.length + pointI : pointI)].z = planePoints[pointI].z;
+        //     }
+        // }
+        //browser_pane.rotation.set(0,0,15);
         mesh.geometry = browser_pane;
     }
 });
